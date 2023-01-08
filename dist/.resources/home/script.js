fe60b23172d0099ec74fe3f60d8bbeddf0710308
Shriram Khandbahale <shriramskhandbahale@gmail.com>
2023-01-08 12:30:55 +0530

    add create room feature

--- dist/.resources/home/script.js
+++ dist/.resources/home/script.js
@@ -1,6 +1,7 @@
 // hide other divs onload
 document.addEventListener("DOMContentLoaded", function () {
   document.querySelector(".join-link-prompt").style.display = "none";
+  document.querySelector(".create-room-prompt").style.display = "none";
 });
 
 // onclick join room
@@ -20,6 +21,25 @@ document
         "block");
 
       let inviteLink = document.getElementById("input-join-link-prompt").value;
+      window.open(inviteLink, "_self");
+    }
+  });
+
+// onclick crete room
+document
+  .querySelector("#choice-btn-create")
+  .addEventListener("click", function (e) {
+    document.querySelector(".choice").style.display = "none";
+    document.querySelector(".create-room-prompt").style.display = "block";
+  });
+
+document
+  .querySelector("#input-crete-room-prompt")
+  .addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+      document.querySelector(".create-room-prompt").style.display = "none";
+
+      let inviteLink = document.getElementById("input-crete-room-prompt").value;
       window.open(inviteLink, "_self");
     }
   });
