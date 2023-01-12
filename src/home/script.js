407508c52f9d55a3c922c0e527a6d8320c079404
Shriram Khandbahale <shriramskhandbahale@gmail.com>
2023-01-12 19:08:54 +0530

    add create room link to frontend

--- src/home/script.js
+++ src/home/script.js
@@ -1,65 +1,98 @@
- // hide other divs onload
- document.addEventListener("DOMContentLoaded", function () {
-   document.querySelector(".join-link-prompt").style.display = "none";
-   document.querySelector(".create-room-prompt").style.display = "none";
- });
+// hide other divs onload
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector(".join-link-prompt").style.display = "none";
+  document.querySelector(".create-room-prompt").style.display = "none";
+  document.querySelector(".processing_animation").style.display = "none";
+  document.querySelector(".generate-invite-link").style.display = "none";
+});
 
- // onclick join room
- document
-   .querySelector("#choice-btn-join")
-   .addEventListener("click", function (e) {
-     document.querySelector(".choice").style.display = "none";
-     document.querySelector(".join-link-prompt").style.display = "block";
-   });
+// onclick join room
+document
+  .querySelector("#choice-btn-join")
+  .addEventListener("click", function (e) {
+    document.querySelector(".choice").style.display = "none";
+    document.querySelector(".join-link-prompt").style.display = "block";
+  });
 
- document
-   .querySelector("#input-join-link-prompt")
-   .addEventListener("keypress", function (e) {
-     if (e.key === "Enter") {
-       document.querySelector(".join-link-prompt").style.display = "none";
-       let container = (document.querySelector(".container").style.display =
-         "block");
+document
+  .querySelector("#input-join-link-prompt")
+  .addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+      document.querySelector(".join-link-prompt").style.display = "none";
+      let container = (document.querySelector(".container").style.display =
+        "block");
 
-       let inviteLink = document.getElementById("input-join-link-prompt").value;
-       window.open(inviteLink, "_self");
-     }
-   });
+      let inviteLink = document.getElementById("input-join-link-prompt").value;
+      window.open(inviteLink, "_self");
+    }
+  });
 
- // onclick crete room
- document
-   .querySelector("#choice-btn-create")
-   .addEventListener("click", function (e) {
-     document.querySelector(".choice").style.display = "none";
-     document.querySelector(".create-room-prompt").style.display = "block";
-   });
+// onclick crete room
+document
+  .querySelector("#choice-btn-create")
+  .addEventListener("click", function (e) {
+    document.querySelector(".choice").style.display = "none";
+    document.querySelector(".create-room-prompt").style.display = "block";
+    document.querySelector(".processing_animation").style.display = "block";
+    displayInviteLink();
+  });
 
+// run file when create room btn is clicked
+document
+  .getElementById("choice-btn-create")
+  .addEventListener("click", function () {
+    fetch("http://localhost:7771/execute-file", {
+      method: "POST",
+    });
+  });
 
- // run file when create room btn is clicked
- document.getElementById('choice-btn-create').addEventListener('click', function () {
-   fetch('http://localhost:7771/execute-file', {
-     method: 'POST'
-   });
+// invite link
+let inviteLink;
+document
+  .getElementById("choice-btn-create")
+  .addEventListener("click", function () {
+    fetch("http://localhost:7771/execute-file")
+      .then((response) => response.text())
+      .then((link) => {
+        inviteLink = link;
+        console.log(`Invite link: http://${inviteLink}`);
+      });
+  });
 
- });
+document
+  .querySelector("#input-crete-room-prompt")
+  .addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+      document.querySelector(".create-room-prompt").style.display = "none";
 
- // invite link
- let inviteLink;
- document.getElementById('choice-btn-create').addEventListener('click', function () {
-   fetch('http://localhost:7771/execute-file')
-     .then((response) => response.text())
-     .then((link) => {
-       inviteLink = link;
-       console.log(`Invite link: http://${inviteLink}`);
-     });
- });
+      let inviteLink = document.getElementById("input-crete-room-prompt").value;
+      window.open(inviteLink, "_self");
+    }
+  });
 
- document
-   .querySelector("#input-crete-room-prompt")
-   .addEventListener("keypress", function (e) {
-     if (e.key === "Enter") {
-       document.querySelector(".create-room-prompt").style.display = "none";
+async function displayInviteLink() {
+  await sleep(10);
+  document.querySelector(".processing_animation").style.display = "none";
+  document.getElementById("invite-link").innerHTML = "http://" + inviteLink;
+  document.querySelector(".generate-invite-link").style.display = "block";
+}
 
-       let inviteLink = document.getElementById("input-crete-room-prompt").value;
-       window.open(inviteLink, "_self");
-     }
-   });
+//copylink
+// document.addEventListener("DOMContentLoaded", function () {
+//   let copy = document.querySelector(".div-invite-link");
+//   copy.querySelector("#copy-icon").addEventListener("click", function () {
+
+//     $("#subtotal").text();
+//     navigator.clipboard.writeText(input.innerText);
+//     copy.classList.add("active");
+//   });
+// });
+
+//join hosted room
+function joinRoom() {
+  window.open(`http://${inviteLink}`, "_self");
+}
+
+async function sleep(s) {
+  return new Promise((resolve) => setTimeout(resolve, s * 1000));
+}
