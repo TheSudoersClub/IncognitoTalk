d01a597045e2c281a28b975073e8fc3e31b948a3
Shriram Khandbahale <shriramskhandbahale@gmail.com>
2023-01-15 14:41:47 +0530

    android create room feature done

--- src/home/script.js
+++ src/home/script.js
@@ -4,47 +4,42 @@ document.addEventListener("DOMContentLoaded", function () {
   document.querySelector(".create-room-prompt").style.display = "none";
   document.querySelector(".processing_animation").style.display = "none";
   document.querySelector(".generate-invite-link").style.display = "none";
+  document.querySelector(".create-room-prompt-mobile").style.display = "none";
 });
-
-
 
 // invite link
 let inviteLink;
 
+//onclick create room
 document
   .getElementById("choice-btn-create")
   .addEventListener("click", function () {
-
-    fetch("http://localhost:7771/execute-file", {
-      method: "POST",
-    });
-
-    fetch("http://localhost:7771/execute-file")
-      .then((response) => response.text())
-      .then((link) => {
-        inviteLink = link;
-        // console.log(`Invite link: http://${inviteLink}`);
-        // alert(inviteLink);
-      });
-
-
     document.querySelector(".choice").style.display = "none";
-    document.querySelector(".create-room-prompt").style.display = "block";
-    document.querySelector(".processing_animation").style.display = "block";
-    displayInviteLink();
 
-    document
-      .querySelector("#input-crete-room-prompt")
-      .addEventListener("keypress", function (e) {
-        if (e.key === "Enter") {
-          document.querySelector(".create-room-prompt").style.display = "none";
-
-          let inviteLink = document.getElementById("input-crete-room-prompt").value;
-          window.open(inviteLink, "_self");
-        }
+    if (
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      )
+    ) {
+      document.querySelector(".create-room-prompt-mobile").style.display =
+        "block";
+    } else {
+      fetch("http://localhost:7771/execute-file", {
+        method: "POST",
       });
-  });
 
+      fetch("http://localhost:7771/execute-file")
+        .then((response) => response.text())
+        .then((link) => {
+          inviteLink = link;
+          // console.log(`Invite link: http://${inviteLink}`);
+          // alert(inviteLink);
+        });
+      document.querySelector(".create-room-prompt").style.display = "block";
+      document.querySelector(".processing_animation").style.display = "block";
+      displayInviteLink();
+    }
+  });
 
 // onclick join room
 document
@@ -76,9 +71,9 @@ async function displayInviteLink() {
 
 //copylink
 function copyToClipboard(id) {
-  let value = document.getElementById(id).innerHTML
+  let value = document.getElementById(id).innerHTML;
   navigator.clipboard.writeText(value);
-  document.querySelector('.copied-message').style.display = "block";
+  document.querySelector(".copied-message").style.display = "block";
 }
 
 //join hosted room
@@ -88,4 +83,18 @@ function joinRoom() {
 
 async function sleep(s) {
   return new Promise((resolve) => setTimeout(resolve, s * 1000));
+}
+
+function copyToClipboardAndroid() {
+  let value = document.getElementById("android-command").innerHTML;
+  // document.write(value)
+  // navigator.clipboard.writeText(value);
+
+  document.querySelector("#create-room-android-copy-button").innerHTML =
+    "Copied";
+
+  setTimeout(() => {
+    document.querySelector(".create-room-prompt-mobile").style.display = "none";
+    document.querySelector(".join-link-prompt").style.display = "block";
+  }, 5000);
 }
