4362903916b3c9b0e0b38d2c5d6885ab895bef01
Shriram Khandbahale <shriramskhandbahale@gmail.com>
2023-01-28 18:41:01 +0530

    add tag feature

--- src/main/scripts/tags.js
+++ src/main/scripts/tags.js
@@ -1,54 +1,11 @@
-//nicknames array
-// let nickNames = ["ram", "shriram", "rahul"];
-// let nickNames = []
-
-//container
-// let tagContainer = document.querySelector('.tags');
-
-
-// document.querySelector('#input-send-message').addEventListener('keydown', function (e) {
-//     if (nickNames.length != 0 && e.key === '@') {
-//         renderClientsList();
-//         tagContainer.style.display = "flex"
-//         sortClientList();
-//     } else if (e.key === ' ' || e.key === "Enter") {
-//         tagContainer.style.display = "none"
-//     }
-// })
-
-// function renderClientsList() {
-//     nickNames.forEach(element => {
-//         let tagElement = document.createElement("span");
-//         tagElement.innerText = element;
-//         tagContainer.appendChild(tagElement);
-//     });
-// }
-
-// function sortClientList(e) {
-//     let string = '@';
-//     document.querySelector('#input-send-message').addEventListener('keydown', function (e) {
-//         string = string + e.key;
-//         string = string.replace("@", "");
-//         renderClientsListSorted(string);
-//     })
-// }
-
-// function renderClientsListSorted(e) {
-//     tagContainer.innerHTML = "";
-//     let tagElement = document.createElement("span");
-//     tagElement.innerText = searchArray(e);
-//     tagContainer.appendChild(tagElement);
-// }
-
-// function searchArray(searchTerm) {
-//     return nickNames.filter(item => item.startsWith(searchTerm));
-// }
-
 let nickNames = ["ram", "shriram", "rahul"];
 let nickNamesFiltered = [];
 let tagTriggered = false;
+let addToValue = false;
 let string = '@';
 let inputField = document.querySelector('#input-send-message');
+let container = document.querySelector('#tags');
+
 renderList(nickNames);
 
 inputField.addEventListener('keydown', function (e) {
@@ -62,7 +19,15 @@ inputField.addEventListener('keydown', function (e) {
     }
 
     if (tagTriggered) {
-        string = string + e.key;
+        if (e.key === 'Backspace') {
+            string = string.slice(0, -1);
+        } else if (e.key === 'Tab') {
+            e.preventDefault();
+            inputField.value = '@' + container.firstChild.innerText;
+        }
+        else {
+            string = string + e.key;
+        }
         string = string.replace("@", "");
         console.log(string);
 
@@ -78,8 +43,9 @@ inputField.addEventListener('keydown', function (e) {
 function filterUnique(array, string) {
     let newArr = [];
     array.forEach(element => {
-        if (element.startsWith(string))
+        if (element.startsWith(string)) {
             newArr.push(element);
+        }
     });
     let nickNamesFIlteredUnique = [...new Set(newArr)];
     renderList(nickNamesFIlteredUnique);
